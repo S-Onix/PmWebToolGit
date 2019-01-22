9c582ffc50b8d7efeb5cf16e016a35cdfabc03f7
S-Onix <msyoo101@hansung.ac.kr>
2019-01-22 16:21:09 +0900

    스프링 컨트롤러 설정 및 MemberVO 완성

--- PmWebTool/src/vo/MemberVO.java
+++ PmWebTool/src/vo/MemberVO.java
@@ -1,5 +1,47 @@
 package vo;
 
 public class MemberVO {
-
+	int mseq;
+	String email;
+	String mname;
+	String password;
+	String mimg;
+	String useyn;
+	
+	public int getMseq() {
+		return mseq;
+	}
+	public void setMseq(int mseq) {
+		this.mseq = mseq;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMimg() {
+		return mimg;
+	}
+	public void setMimg(String mimg) {
+		this.mimg = mimg;
+	}
+	public String getUseyn() {
+		return useyn;
+	}
+	public void setUseyn(String useyn) {
+		this.useyn = useyn;
+	}
 }
