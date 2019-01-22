c1ae9bff14f1e28abe442d89a0f5f3a3ca17bc79
S-Onix <msyoo101@hansung.ac.kr>
2019-01-22 12:35:14 +0900

    스프링 기본적인 틀 설정 및 Member 관련 클래스 생성

---
+++ PmWebTool/src/dao/MemberDAO.java
@@ -0,0 +1,67 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import vo.MemberVO;
+
+public class MemberDAO {
+	DataSource dataSource;
+	
+	
+	//객체 주입 필요함
+	public MemberDAO(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	
+	/*
+	 * CRUD 필요
+	 * 멤버 생성
+	 * 멤버 읽기
+	 * 멤버 업데이트
+	 * 멤버 삭제
+	 * */
+	
+	public int insert(MemberVO member) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = -1;
+		try {
+			String sql = "INSERT INTO MEMBER() VALUES()";
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			// 필요한 데이터 넣어줘야함
+			return pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
+	public ArrayList<MemberVO> selectByTeam(int tseq){
+		ArrayList members = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return members;
+	}
+	
+	
+}
